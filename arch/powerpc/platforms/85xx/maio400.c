bc042f04dae9e6df99b1ea13eea7bd83f6ce07ad
Mark Marshall <Mark.Marshall@omicron.at>
2015-05-20 14:50:46 +0200

    maio400: Add a MAIO-400 machine
    The MAIO-400 machine is based on the code in mpc85xx_rdb.c and
    mpc85xx_ds.c  The difference is that for the MAIO-400 we attempt
    to enter a very low power mode after power_off.  This is important
    because one of the reasons that we may have powered off was due to
    an over-temperature condition, and it is important to try to lower
    the temperature as quickly as possible.
    
    The swiotlb support is required for the maio pcie driver - the FPGA
    cannot access memory above 1 Gig, so we need to enable SWIOTLB if
    needed.
    
    Signed-off-by: Mark Marshall <mark.marshall@omicron.at>

---
+++ arch/powerpc/platforms/85xx/maio400.c
@@ -0,0 +1,189 @@
+/*
+ * MAIO-400 Board Setup
+ *
+ * Copyright 2013 OMICRON electronics GmbH.
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
+#include <linux/stddef.h>
+#include <linux/kernel.h>
+#include <linux/pci.h>
+#include <linux/kdev_t.h>
+#include <linux/delay.h>
+#include <linux/seq_file.h>
+#include <linux/interrupt.h>
+#include <linux/of_platform.h>
+#include <linux/memblock.h>
+
+#include <asm/time.h>
+#include <asm/machdep.h>
+#include <asm/pci-bridge.h>
+#include <mm/mmu_decl.h>
+#include <asm/prom.h>
+#include <asm/udbg.h>
+#include <asm/mpic.h>
+#include <asm/swiotlb.h>
+#include <linux/fsl/guts.h>
+
+#include <sysdev/fsl_soc.h>
+#include <sysdev/fsl_pci.h>
+#include "smp.h"
+
+#undef DEBUG
+
+#ifdef DEBUG
+#define DBG(fmt, args...) printk(KERN_ERR "%s: " fmt, __func__, ## args)
+#else
+#define DBG(fmt, args...)
+#endif
+
+void __init maio400_pic_init(void)
+{
+	struct mpic *mpic = mpic_alloc(NULL, 0, MPIC_BIG_ENDIAN | 
+				       MPIC_SINGLE_DEST_CPU,
+				       0, 256, " OpenPIC  ");
+
+	BUG_ON(mpic == NULL);
+
+	mpic_init(mpic);
+}
+
+/*
+ * Setup the architecture
+ */
+#ifdef CONFIG_SMP
+extern void __init mpc85xx_smp_init(void);
+#endif
+
+static void __init maio400_setup_arch(void)
+{
+	if (ppc_md.progress)
+		ppc_md.progress("maio400_setup_arch()", 0);
+
+	mpc85xx_smp_init();
+
+	swiotlb_detect_4g();
+	fsl_pci_assign_primary();
+
+	printk(KERN_INFO "MAIO-400 board from OMICRON electronics GmbH.\n");
+}
+
+/* Returns a pointer to the GUTS */
+static struct ccsr_guts __iomem *get_p2020_guts(void)
+{
+	struct device_node *guts_np = NULL;
+	struct ccsr_guts __iomem *guts;
+
+	/* Map the global utilities registers. */
+	guts_np = of_find_compatible_node(NULL, NULL, "fsl,p2020-guts");
+	if (!guts_np) {
+		pr_err("maio400: missing global utilties device node\n");
+		return NULL;
+	}
+
+	guts = of_iomap(guts_np, 0);
+	of_node_put(guts_np);
+	if (!guts) {
+		pr_err("maio400: could not map global utilties device\n");
+		return NULL;
+	}
+
+	return guts;
+}
+
+/* Bits from guts->powmgtcsr */
+#define PMCSR_MSK       (0xf << 28)
+#define PMCSR_SLP	(1 << 17)
+#define PMCSR_SLPING	(1 << 1)
+
+/* Switch off all peripherals and put the CPU's into sleep mode */
+static __attribute__((noreturn)) void do_halt(void)
+{
+	struct ccsr_guts __iomem *guts = get_p2020_guts();
+
+	if (guts) {
+		/* Switch off everything */
+		out_be32(&guts->devdisr, 0xffffffff);
+		out_be32(&guts->devdisr2, 0xffffffff);
+
+		/* Disable clocks in sleep mode */
+		out_be32(&guts->pmcdr, 0xffffffff);
+	}
+
+	local_irq_disable();
+	while (1) {
+		if (guts)
+			/* Enable sleep mode, don't allow interrupts
+			 * or Critical interrupts to wake either
+			 * core. */
+			out_be32(&guts->powmgtcsr, PMCSR_MSK | PMCSR_SLP);
+	}
+}
+
+/* Called from machine_power_off in arch/powerpc/kernel/setup-common.c */
+/*static void maio400_power_off(void)
+{
+#ifdef CONFIG_SMP
+	smp_send_stop();
+#endif
+	printk(KERN_EMERG "%s: System Halted, OK to turn off power\n", __func__);
+	do_halt();
+}
+*/
+
+/* Called from machine_halt in arch/powerpc/kernel/setup-common.c */
+		     static __attribute__((noreturn)) void maio400_halt(void)
+{
+#ifdef CONFIG_SMP
+	smp_send_stop();
+#endif
+	printk(KERN_EMERG "%s: System Halted, OK to turn off power\n", __func__);
+	do_halt();
+}
+
+static struct of_device_id __initdata maio400_ids[] = {
+	{ .type = "soc", },
+	{ .compatible = "soc", },
+	{ .compatible = "simple-bus", },
+	{ .compatible = "gianfar", },
+	{},
+};
+
+static int __init maio400_publish_devices(void)
+{
+	return of_platform_bus_probe(NULL, maio400_ids, NULL);
+}
+machine_device_initcall(maio400, maio400_publish_devices);
+
+/*
+ * Called very early, device-tree isn't unflattened
+ */
+static int __init maio400_probe(void)
+{
+	unsigned long root = of_get_flat_dt_root();
+
+	if (of_flat_dt_is_compatible(root, "omicron,maio400"))
+		return 1;
+	return 0;
+}
+
+define_machine(maio400) {
+	.name			= "MAIO-400",
+	.probe			= maio400_probe,
+	.setup_arch		= maio400_setup_arch,
+	.init_IRQ		= maio400_pic_init,
+#ifdef CONFIG_PCI
+	.pcibios_fixup_bus	= fsl_pcibios_fixup_bus,
+#endif
+	.get_irq		= mpic_get_irq,
+    //	.restart		= fsl_rstcr_restart,
+	.calibrate_decr		= generic_calibrate_decr,
+	.progress		= udbg_progress,
+
+    //	.power_off              = maio400_power_off,
+	.halt                   = maio400_halt,
+};
